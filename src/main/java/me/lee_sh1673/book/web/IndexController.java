a5eceabff5369c39cf90595d42d38a77629a384b
LEE-sh1673 <lsh901673@gmail.com>
2022-12-26 22:02:39 +0900

    feat: 게시글 삭제/수정 화면 추가

--- src/main/java/me/lee_sh1673/book/web/IndexController.java
+++ src/main/java/me/lee_sh1673/book/web/IndexController.java
@@ -2,9 +2,11 @@ package me.lee_sh1673.book.web;
 
 import lombok.RequiredArgsConstructor;
 import me.lee_sh1673.book.service.posts.PostsService;
+import me.lee_sh1673.book.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -22,5 +24,12 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(final @PathVariable Long id, final Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
--- src/main/java/me/lee_sh1673/book/web/PostsApiController.java
+++ src/main/java/me/lee_sh1673/book/web/PostsApiController.java
@@ -5,6 +5,7 @@ import me.lee_sh1673.book.service.posts.PostsService;
 import me.lee_sh1673.book.web.dto.PostsResponseDto;
 import me.lee_sh1673.book.web.dto.PostsSaveRequestDto;
 import me.lee_sh1673.book.web.dto.PostsUpdateRequestDto;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,5 +34,11 @@ public class PostsApiController {
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
     }
 }

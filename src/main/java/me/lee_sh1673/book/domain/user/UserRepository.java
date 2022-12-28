a5aaa44d5c1b63480b32c870eab1513da6c79794
LEE-sh1673 <lsh901673@gmail.com>
2022-12-28 23:15:24 +0900

    feat: User 엔티티 관련 코드 작성

---
+++ src/main/java/me/lee_sh1673/book/domain/user/UserRepository.java
@@ -0,0 +1,9 @@
+package me.lee_sh1673.book.domain.user;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}

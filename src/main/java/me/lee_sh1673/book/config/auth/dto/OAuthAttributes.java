71b23c5b1ad5a13a6b91f628bac12194d44f2722
LEE-sh1673 <lsh901673@gmail.com>
2022-12-31 16:35:31 +0900

    feat: Naver 로그인 기능 추가

--- src/main/java/me/lee_sh1673/book/config/auth/dto/OAuthAttributes.java
+++ src/main/java/me/lee_sh1673/book/config/auth/dto/OAuthAttributes.java
@@ -9,15 +9,15 @@ import me.lee_sh1673.book.domain.user.User;
 @Getter
 public class OAuthAttributes {
 
-    private Map<String, Object> attributes;
+    private final Map<String, Object> attributes;
 
-    private String nameAttributeKey;
+    private final String nameAttributeKey;
 
-    private String name;
+    private final String name;
 
-    private String email;
+    private final String email;
 
-    private String picture;
+    private final String picture;
 
     @Builder
     public OAuthAttributes(final Map<String, Object> attributes,
@@ -35,6 +35,10 @@ public class OAuthAttributes {
         final String userNameAttributeName,
         final Map<String, Object> attributes) {
 
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -46,6 +50,20 @@ public class OAuthAttributes {
             .email((String) attributes.get("email"))
             .picture((String) attributes.get("picture"))
             .attributes(attributes)
+            .nameAttributeKey(userNameAttributeName)
+            .build();
+    }
+
+    private static OAuthAttributes ofNaver(final String userNameAttributeName,
+        final Map<String, Object> attributes) {
+
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+            .name((String) response.get("name"))
+            .email((String) response.get("email"))
+            .picture((String) response.get("picture"))
+            .attributes(response)
             .nameAttributeKey(userNameAttributeName)
             .build();
     }
